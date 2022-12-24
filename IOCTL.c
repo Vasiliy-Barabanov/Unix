b6b66aa5db25da6f6b9c763334aedbaeb61bb3a9
Vasiliy-Barabanov <72937599+Vasiliy-Barabanov@users.noreply.github.com>
2022-12-24 10:26:40 +0300

    Update IOCTL.c

--- IOCTL.c
+++ IOCTL.c
@@ -1,81 +1,94 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <asm/uaccess.h>
-#include <sys/ioctl.h>
-#include <asm/ioctl.h>
+#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
+#include <linux/module.h>           // Core header for loading module into the kernel
+#include <linux/kernel.h>   
+#include <linux/fs.h>
+#include <linux/uaccess.h>  
+#include <linux/slab.h>  
+#include <linux/cdev.h>
 #include <linux/ioctl.h>
-#include <stdio.h>
+#include <mydriverio.h>
+ 
+MODULE_LICENSE("GPL");              ///< The license type -- this affects function visibility
+MODULE_AUTHOR("Alex Maximov");      ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple Linux module");  ///< The description -- see modinfo
+MODULE_VERSION("1.0");              
 
-/*
-#ifndef ”mydriverio.h”
-#define ”mydriverio.h”
-#endif
-*/
+ 
+/** @brief The module initialization function
+ *  The static keyword restricts the visibility of the function to within this C file. The __init
+ *  macro means that  the function is only used at initialization and that it can be discarded and its memory freed up after that point.
+ *  @return returns 0 if successful
+ */
+ 
+static char kbuf[1000] = "hello";
+static dev_t first;
+static unsigned int count=1;
+static int my_major =300, my_minor=0;
+static struct cdev *my_cdev;
 
+#define MYDEV_NAME "mychrdev"
+#define KBUF_SIZE (size_t) ((10)* PAGE_SIZE) 
+ 
 
+static int mychrdev_open(struct inode *inode, struct file *file){
+   return 0;
+}
+static int mychrdev_release(struct inode *inode, struct file *file){
+   return 0;
+}
+static ssize_t mychrdev_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos){ //доставка в юзерспейс
+       if (lbuf>1000)
+         lbuf = 1000;
+       copy_to_user(buf, kbuf,lbuf);
+       printk(KERN_INFO "Read dev");  
+   return lbuf;
+}
+static ssize_t mychrdev_write(struct file *file, const char __user *buf, size_t lbuf, loff_t *ppos){
+   return 0;
+}
 
-int device_open (struct inode *myinode, struct file *myflie) {
-    return 0;
+static inline long foo_unlocked_ioctl(struct file *fp,unsigned long cmd, unsigned long arg){
+	int ret=0;
+	switch(cmd){
+		case IOC_GET;
+		printk("a \n");
+		break;
+		case F;
+		printk("b \n");
+		break;
+	}
+	return 0;
 }
 
 
-
-
-int device_release (struct inode *, struct file *) {
-    return 0;
-}
-
-
-
-static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t * offset) {
-int bytes_read = 0; /* Number of bytes actually written to the buffer. If we're at the end of the message, * return 0 signifying end of file */
-if (*msg_Ptr == 0) return 0;  /* Actually put the data into the buffer */
-while (length && *msg_Ptr) {
-/** The buffer is in the user data segment, not the kernel
-* segment so "*" assignment won't work. We have to use
-* put_user which copies data from the kernel data segment to
-* the user data segment. */
-put_user(*(msg_Ptr++), buffer++);
-length--;
-bytes_read++;
-}
-/* Most read functions return the number of bytes put into the buffer */
-return bytes_read;
-}
-
-
-
-long myioctl (struct file *, unsigned int, unsigned long){
-    return 0;
-}
-
-
-
-struct file_operations fops = {
-.read = device_read,
-.unlocked_ioctl = myioctl,
-.open = device_open,
-.release = device_release
+static const struct file_operations mycdev_fops={
+   .owner=THIS_MODULE,
+   .unlocked_ioctl=foo_unlocked_ioctl,
+   .read=mychrdev_read,
+   .write=mychrdev_write,
+   .open=mychrdev_open,
+   .release=mychrdev_release,
 };
 
 
 
-int init_module(void) {
-Major = register_chrdev(0, DEVICE_NAME, &fops);
-if (Major < 0) {
-printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-return Major;
-}
-printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
-printk(KERN_INFO "the driver, create a dev file with\n");
-printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major)
-return 0;
+static int __init simple_init(void){
+   //kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+   first = MKDEV (my_major,my_minor);
+   register_chrdev_region(first,count,MYDEV_NAME);
+   my_cdev = cdev_alloc();
+   cdev_init(my_cdev, &mycdev_fops);
+   cdev_add(my_cdev,first,count);
+   return 0;
 }
 
-
-void cleanup_module(void) {
-unregister_chrdev(Major, DEVICE_NAME);
-printk(KERN_ALERT "Сleanup_module OK \n");
+ 
+/** @brief The modul cleanup function
+ */
+static void __exit simple_exit(void){
+   unregister_chrdev_region(first,count);
+   printk(KERN_INFO "Goodbye from the mai 307 team!\n");
 }
+ 
+module_init(simple_init);
+module_exit(simple_exit);
